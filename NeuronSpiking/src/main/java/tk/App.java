15acb37fc196d823e051837c2cdaf1dd5762e031
Tadeusz Karczynski <tadeusz.karczynski@gmail.com>
2016-05-18 11:53:32 +0200

    Działa4Input

---
+++ NeuronSpiking/src/main/java/tk/App.java
@@ -0,0 +1,35 @@
+package tk;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.applet.Applet;
+
+/**
+ * Created by Tadzik on 2016-05-18.
+ */
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("Widok.fxml"));
+            Scene scene = new Scene(root, 800, 700);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Neuron");
+            primaryStage.show();
+
+
+//        primaryStage.setScene(new Scene(createContent()));
+//        primaryStage.setTitle("SpikingNeuron");
+//        primaryStage.show();
+
+
+        }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    }
+
---
+++ NeuronSpiking/src/main/java/tk/Controller.java
@@ -0,0 +1,169 @@
+package tk;
+
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.Random;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Tadzik on 2016-05-16.
+ */
+
+////////////////
+/////////
+
+public class Controller implements Initializable {
+
+    @FXML
+    LineChart<Number, Number> input1LineChart;
+    @FXML
+    LineChart<Number, Number> input2LineChart;
+    @FXML
+    LineChart<Number, Number> input3LineChart;
+    @FXML
+    LineChart<Number, Number> input4LineChart;
+    @FXML
+    LineChart<Number, Number> outputLineChart;
+
+    public void initialize(URL location, ResourceBundle resources) {
+        input1LineChart.setTitle("");
+        animation = new Timeline();
+        animation.getKeyFrames()
+                .add(new KeyFrame(Duration.millis(1000),
+                        (ActionEvent actionEvent) -> plotTime()));
+        animation.setCycleCount(Animation.INDEFINITE);
+        createContent();
+        play();
+
+    }
+
+    @FXML
+    private NumberAxis xAxisLC1;
+
+    @FXML
+    private NumberAxis xAxisLC2;
+    @FXML
+    private NumberAxis xAxisLC3;
+    @FXML
+    private NumberAxis xAxisLC4;
+
+    XYChart.Series s1 = new XYChart.Series<Number, Number>();
+
+    XYChart.Series s2 = new XYChart.Series<Number, Number>();
+    XYChart.Series s3 = new XYChart.Series<Number, Number>();
+    XYChart.Series s4 = new XYChart.Series<Number, Number>();
+    private Timeline animation;
+
+    private double sequence = 0;
+
+    private double y = 10;
+
+    private final int MAX_DATA_POINTS = 30, MAX = 1, MIN = 0;
+
+    public Controller() {
+
+        // create timeline to add new data every 60th of second
+    }
+
+    public void createContent() {
+
+        xAxisLC1 = new NumberAxis(0, MAX_DATA_POINTS + 1, 2);
+        xAxisLC2 = new NumberAxis(0, MAX_DATA_POINTS + 1, 2);
+        xAxisLC3 = new NumberAxis(0, MAX_DATA_POINTS + 1, 2);
+        xAxisLC4 = new NumberAxis(0, MAX_DATA_POINTS + 1, 2);
+        final NumberAxis yAxis = new NumberAxis(MIN - 1, MAX + 1, 1);
+
+        // setup chart
+        input1LineChart.setAnimated(true);
+        input1LineChart.setLegendVisible(false);
+        // setup chart
+        input2LineChart.setAnimated(true);
+        input2LineChart.setLegendVisible(false);
+        // setup chart
+        input3LineChart.setAnimated(true);
+        input3LineChart.setLegendVisible(false);
+        // setup chart
+        input4LineChart.setAnimated(true);
+        input4LineChart.setLegendVisible(false);
+
+        // xAxisLC1.setLabel("X Axis");
+        xAxisLC1.setForceZeroInRange(false);
+        xAxisLC2.setForceZeroInRange(false);
+        xAxisLC3.setForceZeroInRange(false);
+        xAxisLC4.setForceZeroInRange(false);
+
+        yAxis.setLabel("Y Axis");
+        yAxis.setTickLabelFormatter(new NumberAxis.DefaultFormatter(yAxis, "$", null));
+
+
+        s1.getData().add(new XYChart.Data<Number, Number>(sequence, y));
+        s2.getData().add(new XYChart.Data<Number, Number>(sequence, y));
+        s3.getData().add(new XYChart.Data<Number, Number>(sequence, y));
+        s4.getData().add(new XYChart.Data<Number, Number>(sequence, y));
+
+        // create some starting data
+        input1LineChart.getData()
+                .add(s1);
+        input2LineChart.getData()
+                .add(s2);
+        input3LineChart.getData()
+                .add(s3);
+        input4LineChart.getData()
+                .add(s4);
+
+    }
+
+    private void plotTime() {
+        s1.getData().add(new XYChart.Data<Number, Number>(sequence, getNextValue()));
+        s2.getData().add(new XYChart.Data<Number, Number>(sequence, getNextValue()));
+        s3.getData().add(new XYChart.Data<Number, Number>(sequence, getNextValue()));
+        s4.getData().add(new XYChart.Data<Number, Number>(sequence, getNextValue()));
+
+        sequence++;
+
+        // after 25hours delete old data
+        if (sequence > MAX_DATA_POINTS) {
+            s1.getData().remove(0);
+        }
+
+        // every hour after 24 move range 1 hour
+        if (sequence > MAX_DATA_POINTS - 1) {
+            xAxisLC1.setLowerBound(xAxisLC1.getLowerBound() + 1);
+            xAxisLC1.setUpperBound(xAxisLC1.getUpperBound() + 1);
+
+            xAxisLC2.setLowerBound(xAxisLC2.getLowerBound() + 1);
+            xAxisLC2.setUpperBound(xAxisLC2.getUpperBound() + 1);
+
+            xAxisLC3.setLowerBound(xAxisLC3.getLowerBound() + 1);
+            xAxisLC3.setUpperBound(xAxisLC3.getUpperBound() + 1);
+
+            xAxisLC4.setLowerBound(xAxisLC4.getLowerBound() + 1);
+            xAxisLC4.setUpperBound(xAxisLC4.getUpperBound() + 1);
+        }
+    }
+
+    private int getNextValue() {
+        Random rand = new Random();
+        return rand.nextInt(2) * 10;
+    }
+
+    public void play() {
+        animation.play();
+    }
+
+
+    public void stop() {
+        animation.pause();
+    }
+
+}
